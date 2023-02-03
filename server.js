cf410573c84213e51fff307a637d7932566c6b6a
Moonbeam <mfzernick@gmail.com>
2023-02-03 00:07:48 -0600

    fixed it omg wow that was scary

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for Notes page
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
